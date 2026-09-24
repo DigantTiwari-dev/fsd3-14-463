251a45a44da95aa13bd4cf0bfcb31de44cf465fc
Digant Tiwari <tdigant5@gamil.com>
2026-09-24 13:25:36 +0530

    lab5 done

--- lab5/app.js
+++ lab5/app.js
@@ -1,11 +1,30 @@
-import express from 'express'
 
+import express from "express";
+const app = express();
 
-const app = express()
-
-app.get("/",(req,res)=>{
+app.get("/", (req, res) => {
     res.send("<h1> Hello Express");
-})
+});
 
-app.listen(3000,()=>console.log("server is running"));
+app.get("/about", (req, res) => {
+    res.send("WE are FSD Developer");
+});
+
+app.get("/login", (req, res) => {
+    res.send({ msg: "user login" });
+});
+
+app.put("/user/update/1", (req, res) => {
+    res.send({ msg: "user update" });
+});
+
+app.delete("/user/1", (req, res) => {
+    res.send({ msg: "remove user 1" });
+});
+
+app.use((req, res) => {
+    res.status(404).send("Not Found");
+});
+
+app.listen(3333, () => console.log("Server is running"));
 
---
+++ lab5/readme.md
@@ -0,0 +1,16 @@
+# Express
+
+
+Fast, unopinioneted, minimalist web framework for node.js
+
+
+1. create npm project `npm init -y`
+2. update package.json
+    `type:module`
+3. install nodemon with `npm i nodemon -D`
+4. install express woth `npm i express`
+5. update package.json 
+```
+    "start": "node app.js",
+    "dev": "nodemon app.js" 
+    ```
